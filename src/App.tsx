338f469698537d877e5fa8fdf231197f83eb8746
manhanh3011 <tranmanhanh3011@gmail.com>
2026-02-07 05:03:15 +0700

    feat: implement protected route and update task status in db.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { Toaster } from "react-hot-toast";
-import { Route, Routes } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
+import { Navigate, Outlet, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import List from "./pages/List";
 import Add from "./pages/Add";
@@ -7,15 +7,28 @@ import Edit from "./pages/Edit";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
+function ProtectedRoute(){
+  const token = localStorage.getItem("token");
+
+  if(!token){
+    toast.error("Vui lòng đăng nhập để sử dụng chức năng");
+    return <Navigate to={"/login"}/>
+  }
+
+  return <Outlet/>
+}
+
 function App() {
   
   return(
     <>
       <Routes>
         <Route path="/" element={<Layout/>}>
-          <Route path="tasks" element={<List/>}/>
-          <Route path="tasks/add" element={<Add/>}/>
-          <Route path="tasks/edit/:id" element={<Edit/>}/>
+          <Route element={<ProtectedRoute/>}>
+            <Route path="tasks" element={<List/>}/>
+            <Route path="tasks/add" element={<Add/>}/>
+            <Route path="tasks/edit/:id" element={<Edit/>}/>
+          </Route>           
           <Route path="login" element={<Login/>}/>
           <Route path="register" element={<Register/>}/>
         </Route>
