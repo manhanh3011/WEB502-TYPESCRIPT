4b004c1b5fe570fc5a01c15cdbe379282e539147
manhanh3011 <tranmanhanh3011@gmail.com>
2026-02-07 04:51:44 +0700

    feat: add routing with Layout component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { Toaster } from "react-hot-toast"
+import { Route, Routes } from "react-router-dom"
+import Layout from "./layouts/Layout"
 
 function App() {
   
   return(
     <>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+        
+        </Route>
+      </Routes>
       <Toaster/>
     </>
   )
